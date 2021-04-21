cc7bc194bc9587cd001f5b41b6fea22ff1a9fa01
Michael <michaelbarneyjunior@gmail.com>
2021-04-21 19:53:24 -0300

    2021 update - use twilio functions

--- index.js
+++ index.js
@@ -1,77 +1,59 @@
-// Get Enviroment Variables
-const accountSid = process.env.accountSid;
-const authToken = process.env.authToken;
-const projectId = process.env.projectId;
+const dialogflow = require("dialogflow");
 
-// Initialization
-const functions = require('firebase-functions');
-const client = require('twilio')(accountSid, authToken);
-const dialogflow = require('dialogflow');
-const sessionClient = new dialogflow.SessionsClient();
+exports.handler = async function (context, event, callback) {
+  let twiml = new Twilio.twiml.MessagingResponse();
 
-// Twilio Webhook Function
-exports.TwilioWebhook = functions.https.onRequest(async (request, response) => {
-  // Get WhatsApp Message Information  
-  const body = request.body;
-  const receivedMsg = body.Body;
-  const userNumber = body.From;
-  const myNumber = request.body.To;
+  const receivedMsg = event.Body;
+  const userNumber = event.From;
 
-  if(receivedMsg){
-    // Configure Dialogflow Session
-    const sessionPath = sessionClient.sessionPath(projectId, userNumber);
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          text: receivedMsg,
-          languageCode: "pt-BR",
-        },
+  if (!receivedMsg) return callback("No message received", twiml);
+
+  // Get response from Dialogflow
+  let diallofglowJsonFilePath = Runtime.getAssets()["/dialogflow.json"].path;
+  const dialogflogSessionClient = new dialogflow.SessionsClient({
+    keyFilename: diallofglowJsonFilePath,
+  });
+  const dialogflowProjectId = process.env.DIALOGFLOW_PROJECT_ID;
+  const dialogflowSessionPath = dialogflogSessionClient.sessionPath(
+    dialogflowProjectId,
+    userNumber
+  );
+  const dialogflowRequestParams = {
+    session: dialogflowSessionPath,
+    queryInput: {
+      text: {
+        text: receivedMsg,
+        languageCode: "pt-BR",
       },
-    };
-  
-    // Get Dialogflow Response
-    try {
-      const fulfillmentMessages = (await sessionClient.detectIntent(request))[0].queryResult.fulfillmentMessages;
-      
-      // Iterate over every message
-      for (const response of fulfillmentMessages){
-        // Send Text Message
-        if(response.text){
-            const responseMsg = response.text.text[0];
-            const body = {
-                from: myNumber,
-                body: responseMsg,
-                to: userNumber
-            }
-            await client.messages.create(body)
-        }
+    },
+  };
+  const dialogflowRequest = await dialogflogSessionClient.detectIntent(
+    dialogflowRequestParams
+  );
+  const dialogflowResponses =
+    dialogflowRequest[0].queryResult.fulfillmentMessages;
 
-        // Send media files
-        if(response.payload){
-          if(response.payload.fields.mediaUrl){
-            const mediaUrl = response.payload.fields.mediaUrl.stringValue;
-            
-            let text = "";
-            
-            if(response.payload.fields.text){
-              text = response.payload.fields.text.stringValue
-            }
-            
-            const body = {
-                from: myNumber,
-                body: text,
-                to: userNumber,
-                mediaUrl,
-            }
-            await client.messages.create(body)
-          }
-        }
+  // Iterate over every message
+  for (const response of dialogflowResponses) {
+    // Texts
+    if (response.text) {
+      const text = response.text.text[0];
+      twiml.message(text);
+    }
+
+    // Payloads
+    if (response.payload) {
+      const fields = response.payload.fields;
+      const payloadMessage = await twiml.message();
+      if (fields.mediaUrl) {
+        const mediaUrl = fields.mediaUrl.stringValue;
+        payloadMessage.media(mediaUrl);
+      }
+      if (fields.text) {
+        const text = fields.text.stringValue;
+        payloadMessage.body(text);
       }
     }
-    catch(e){
-      console.log(e)
-    }
   }
-  response.status(200).send("Sent!");
-});
+  return callback(null, twiml);
+};
